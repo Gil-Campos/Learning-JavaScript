094c6775e01f525a95d2d49993b335e4f6f72e14
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-04-03 21:20:20 -0600

    Created deposit function checking if it is a number using the NaN() function and used a while loop till the user enters valid data

--- 1-Learning-JS/project.js
+++ 1-Learning-JS/project.js
@@ -1,3 +1,4 @@
+// TODO LIST:
 // 1. Deposit some money
 // 2. Determine number of lines to bet on
 // 3. Collect a bet amount
@@ -8,8 +9,19 @@
 
 const prompt = require("prompt-sync")();
 
+// Deposit some money function
 const deposit = () => {
+  while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+        return numberDepositAmount;
+    }
+  }
 };
 
-deposit();
+const depositAmount = deposit();
+console.log(depositAmount);
